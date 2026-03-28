35024951829abbe375f18e50700be0de116396d2
unknown <azatynysov@gmail.com>
2026-03-28 14:55:53 +0600

    please, add this code

---
+++ StringConectionSQLC#/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
+public class Connection
+{
+    public void StrinngConection()
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["LeessonDB"].ConnectionString))
+        {
+            sqlConnection.Open();
+            Console.WriteLine("Finaly, its worked!");
+            Console.WriteLine("Can i change it?");
+            Console.ReadLine();
+        }
+    }
+}
+public static class Test
+{
+    public static void Main(string[] args)
+    {
+        var test = new Connection();
+        test.StrinngConection();
+    }
+}
+
+
